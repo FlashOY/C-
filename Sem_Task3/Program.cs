12a7598f36d752643b4dca64bb54370893ca3bd9
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-03 22:45:22 +0300

    Added Sem1_Task3_adding_new_fiches

--- Sem_Task3/Program.cs
+++ Sem_Task3/Program.cs
@@ -8,9 +8,16 @@
 Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine()); 
 
+if (num >= 100 && num <= 999) //if (num > 99 && num < 1000)
+{
 int firstdigit = num / 100; //456/10=4.56
 int seconddigit = num %10; //456 = 450 + 6 = 6; 
 
 int result = firstdigit + seconddigit; 
 
-Console.WriteLine ("Результат сложения первой и последней цифры числа = " +result); 
+Console.WriteLine ($"Результат сложения первой и последней цифры числа = " + {result}); 
+} else 
+{
+    "Введённое число не является трёхзначным"
+}
+
