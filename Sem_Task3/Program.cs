c8ff932f00b2b55132f875b450c1f62ac43dadde
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-03 22:31:04 +0300

    Added Sem1_Task3_result

--- Sem_Task3/Program.cs
+++ Sem_Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное целое число и на выходе показывает
+// сумму первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+int firstdigit = num / 100; //456/10=4.56
+int seconddigit = num %10; //456 = 450 + 6 = 6; 
+
+int result = firstdigit + seconddigit; 
+
+Console.WriteLine ("Результат сложения первой и последней цифры числа = " +resut); 
