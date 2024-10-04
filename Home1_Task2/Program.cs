e7faf69d66d0ad8daa41b37be91291a08e2d0c69
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-04 07:52:46 +0300

    Added Home1_Task2

---
+++ Home1_Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задание 2
+// Поиск среднего из трех чисел
+// Описание: Напишите метод, который принимает на вход три числа и возвращает
+// среднее из этих чисел (то есть не самое большое и не самое маленькое).
+// Пример использования: На входе:
+// ● a: 5
+// ● b: 3
+// ● c: 8
+// На выходе:
+// ● 5
+// На входе:
+// ● a: 1
+// ● b: 9
+// ● c: 7
+// На выходе:
+// ● 7
+
+Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+if ((a >= b && a <= c) || (a >= c && a <= b))
+{
+     Console.WriteLine(a);
+}
+else if ((b >= a && b <= c) || (b >= c && b <= a))
+{
+     Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(c);
+}
