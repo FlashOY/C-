97fb0e556608cf4529f77318ce459586aa83dc08
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-16 21:49:15 +0300

    Added Home3_Task1

---
+++ Home3_Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 3, 8, 19, 3, 8 };
+int num = 8;
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+Console.WriteLine(isExistNum ? "Да" : "Нет");
