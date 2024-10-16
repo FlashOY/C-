e2c4a9ca38dd8329ef03f3203b039a2e13cda6fd
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-16 21:53:35 +0300

    Added Home3_Task3

---
+++ Home3_Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum += array[i];
+}
+
+double average = (double)sum / array.Length;
+Console.WriteLine(average);
