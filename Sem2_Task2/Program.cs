e5ba8c08c8093c1eb411c6e450d163b7c41d0fad
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-10-21 19:57:54 +0300

    Added Home4_Task1

---
+++ Sem2_Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num <= 999)
+{
+int secondDigit = num / 10 % 10;
+int lastDigit = num % 10;
+
+int result = secondDigit;
+int count = 1;
+
+while (count < lastDigit)
+{
+result = result * secondDigit; // result *= secondDigit
+count++;
+}
+
+Console.WriteLine(result);
+}
+else
+{
+Console.WriteLine("Ошибка ввода!");
+}
