90dfbd2e12b032028c3e95e3bb6313b0d2cfb075
Flash_OY <oleksandr.yasnyi@gmail.com>
2024-11-23 18:07:15 +0200

    Added Home7_Task3

---
+++ Home7_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Вывод элементов массива в обратном порядке
+// Описание: Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Используйте рекурсию и не используйте циклы.
+// Пример:
+// ● Вход: {1, 2, 3, 4, 5}
+// ● Выход: 5, 4, 3, 2, 1
+// ● Вход: {10, 20, 30, 40}
+// ● Выход: 40, 30, 20, 10
+
+
+
+public class Task3
+{
+    public static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0) return;
+        Console.Write(array[index] + (index > 0 ? ", " : ""));
+        PrintArrayReverse(array, index - 1);
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        PrintArrayReverse(array, array.Length - 1);
+    }
+}
